b994390d41ac907213ec5ac89eea16cb6a0515d0
Rahulchandi <rahulchandiramani@gmail.com>
2015-08-25 14:17:25 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,7 @@
 
+$("#main").append("Rahulchandi");
 var awesomeThoughts=("I am Rahul and I am AWESOME!");
 var funThoughts=
   awesomeThoughts.replace("AWESOME","FUN");
-$(#main).append(funThoughts);
+$("#main").append(funThoughts);
  
